a227bab6c9e959dbf10d014b536d62f620a9ae3c
xkwg <xkwg@163.com>
2020-07-21 17:55:25 +0800

    函数式编程回顾

--- code/01functionalProgramming/10-柯里化模拟.js
+++ code/01functionalProgramming/10-柯里化模拟.js
@@ -10,22 +10,21 @@ function sum (a,b,c){ //三个参数就是三元的意思
 
 //第一步 观察lodash中柯里化接受的参数 是一个函数
 function curry (fn) {
-    return function cry(...arg) {
-        // console.log(fn.length);
-        // console.log(arg);
-        //如果满足参数个数 直接返回
-        if(arg.length >= fn.length){
-            return fn.apply(this,arg)
-        }else{
-            //不满足的话返回新的函数
-            return function(...arguments){
-                //第一次传进来的arg被缓存了,这里组合起来 再给cry判断一遍 如此重复直到满足为止
-                let ary = arg.concat(arguments)
-                console.log(ary);
-                return cry(...ary)
-            }
-        }
-    }
+    //第二步 lodash中返回的是一个接收参数的函数
+  return function cry(...arg) {
+      //第三部 判断是否满足参数个数
+      if(arg.length >= fn.length){
+            //满足=>直接返回
+          return fn.apply(this,arg)
+      }else{
+          //不满足=>返回新的函数
+          return function(...arguments){
+              //第一次传进来的arg被缓存了,这里组合起来 再给cry判断一遍 如此重复直到满足为止
+              let ary = arg.concat(arguments)
+              return cry(...ary)
+          }
+      }
+  }
 }
 
 const curryFun = curry(sum);
