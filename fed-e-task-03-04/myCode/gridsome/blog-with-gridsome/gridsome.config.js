e1718e9cebe6f85eb7bab392afb2726266c2cf1a
xkwg <xkwg@163.com>
2020-11-20 16:27:55 +0800

    GridSome搭建自己的SSR静态网站

--- fed-e-task-03-04/myCode/gridsome/blog-with-gridsome/gridsome.config.js
+++ fed-e-task-03-04/myCode/gridsome/blog-with-gridsome/gridsome.config.js
@@ -13,6 +13,34 @@ module.exports = {
         typeName: 'BlogPost',
         path: './content/blog/**/*.md',
       }
+    },
+    {
+      use: '@gridsome/source-strapi',
+      options: {
+        apiURL: 'http://localhost:1337',
+        queryLimit: 1000, // 默认查询100条数据
+        contentTypes: ['post','tag'],
+        typeName:'Strapi',
+        // singleTypes: ['impressum'],//单个节点
+        // Possibility to login with a Strapi user,
+        // when content types are not publicly available (optional).
+        loginData: {
+          identifier: '',
+          password: ''
+        }
+      }
     }
   ],
+  templates:{
+    StrapiPost: [
+      {
+        path: '/post/:id',
+        component:'./src/templates/Post.vue'
+      }
+    ],
+    // StrapiTag:[
+    //     path: '/post/:id',
+    //     component:'./src/templates/Tag.vue'
+    // ]
+  }
 }
